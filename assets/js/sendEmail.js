231bbee69814f8038ee8b94bbb228d62415658af
kartikey <kartikey@anubudh.com>
2023-07-18 23:33:11 +0530

    fix: git hub username & email sending

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,17 +1,16 @@
 function sendMail(contactForm) { // Passing in the form as a parameter 
-    // emailjs.send("outlook", "paulbf", { // x3 keys below match the email parameters
-    //     "from_name": contactForm.name.value,
-    //     "from_email": contactForm.emailaddress.value,
-    //     "Project_Request": contactForm.projectsummary.value
-    // })
-    // .then( // Continuing with the 'Promise Approach > When & Then'
-    //     function(response) {
-    //         console.log("SUCCESS", response);
-    //     },
-    //     function(error) {
-    //         console.log("FAILED", error);
-    //     }
-    // );
-    // return false;  // To block from loading a new page
-    return true;
+    emailjs.send("service_j50w5oq","template_m3k8pfq",{// x3 keys below match the email parameters
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "Project_Request": contactForm.projectsummary.value
+    })
+    .then( // Continuing with the 'Promise Approach > When & Then'
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
 }
