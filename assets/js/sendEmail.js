b490a7d4c3deaa6c60d46dbfa06c569aa7abf78d
Kartikey Gupta <139566505+ownkartikey@users.noreply.github.com>
2025-07-07 17:09:45 +0530

    updating emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) { // Passing in the form as a parameter 
-    emailjs.send("service_j50w5oq","template_m3k8pfq",{// x3 keys below match the email parameters
+    emailjs.send("service_vgmng8j","template_j71v6xs",{// x3 keys below match the email parameters
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "Project_Request": contactForm.projectsummary.value
